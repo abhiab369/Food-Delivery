e28211abd49e53ccbf48aae18a52fd00fd7e62dd
abhishek-ccs-at-054280872609 <abhicomputer0909@gmail.com>
2024-10-05 15:49:07 +0530

    Added Explore menu, context, food display and food items

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
         <div className='header'>
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishesh crafted with the finest ingreadients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
+                <p>Choose from a diverse menu featuring a delectable array of dishesh crafted with the finest ingreadients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
                 <button>View Menu</button>
             </div>
         </div>
