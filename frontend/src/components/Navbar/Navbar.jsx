006e00cd16a6f41e872fb6a1ef0d758ebe833367
abhishek-ccs-at-054280872609 <abhicomputer0909@gmail.com>
2024-10-19 19:43:21 +0530

    Added login page, added functionality in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
-
-export const Navbar = () => {
+export const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home")
     return (
         <div className='navbar'>
             <img src={assets.logo} alt='' className="logo" />
             <ul className="navbar-menu">
-                <li className={menu === 'home' ? 'active' : ''} onClick={() => setMenu("home")}>home</li>
-                <li className={menu === 'menu' ? 'active' : ''} onClick={() => setMenu("menu")}>menu</li>
-                <li className={menu === 'mobile-app' ? 'active' : ''} onClick={() => setMenu("mobile-app")}>mobile-app</li>
-                <li className={menu === 'contact-us' ? 'active' : ''} onClick={() => setMenu("contact-us")}>contact us</li>
+                <Link to='/' className={menu === 'home' ? 'active' : ''} onClick={() => setMenu("home")}>home</Link>
+                <a href='#explore-menu' className={menu === 'menu' ? 'active' : ''} onClick={() => setMenu("menu")}>menu</a>
+                <a href='#app-download' className={menu === 'mobile-app' ? 'active' : ''} onClick={() => setMenu("mobile-app")}>mobile-app</a>
+                <a href='#footer' className={menu === 'contact-us' ? 'active' : ''} onClick={() => setMenu("contact-us")}>contact us</a>
             </ul>
             <div className='navbar-right'>
                 <img src={assets.search_icon} alt="" />
@@ -20,7 +20,7 @@ export const Navbar = () => {
                     <img src={assets.basket_icon} alt="" />
                     <div className='dot'></div>
                 </div>
-                <button>sign in</button>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
         </div>
     )
