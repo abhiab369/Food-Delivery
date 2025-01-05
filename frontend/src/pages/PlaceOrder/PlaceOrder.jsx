1e54c03279b3a42d3e4ae9fdbb29702856923ece
abhishek-ccs-at-054280872609 <abhicomputer0909@gmail.com>
2025-01-05 13:37:56 +0530

    Added My orders

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState,useEffect } from 'react'
 import { StoreContext } from '../../context/StoredContext'
 import './PlaceOrder.css'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const PlaceOrder = () => {
 
@@ -36,7 +37,7 @@ export const PlaceOrder = () => {
     }
     let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } })
     if (response.data.success) {
-      const {session_url} = response.data;
+      const { session_url } = response.data;
       window.location.replace(session_url)
     }
     else {
@@ -49,6 +50,16 @@ export const PlaceOrder = () => {
     const value = event.target.value;
     setData(data => ({ ...data, [name]: value }))
   }
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart")
+    }
+    else if (getTotalCartAmount() === 0) {
+      navigate("/cart")
+    }
+  }, [token])
 
   return (
     <form action="" className='place-order' onSubmit={placeOrder}>
